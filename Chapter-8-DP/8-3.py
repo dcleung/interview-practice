7e9dd962d7942adee7b259a8d35395c9cfb904b1
dcleung <derex.leung@gmail.com>
2017-12-26 15:07:32 -0500

    Fixed 8-3
    Added reasoning for running time.

--- Chapter-8-DP/8-3.py
+++ Chapter-8-DP/8-3.py
@@ -4,15 +4,26 @@ import unittest
 # Given a sorted array of distinct integers, write a method to find a magic index, if one exists, in array A.
 
 # We leverage the invariant that our input array is sorted and that every entry is an integer.
-# This means, if we encounter an entry that is greater than index, every entry afterwards is > index.
-# This runs in O(n) time, because worst case, our entry could be the last entry in the array.
+# Start at median, then compare, and recurse depending if the median greater/less than the index.
+
+# The running time is O(log n) because we will ever search one half of the array in the recursive call,
+# thus the recurrence is T(n) = T(n/2) + 1 for recursing and comparing.  This results in O(log n).
+
+def recurse_find_index(array, lo, hi):
+    index = (lo + hi) / 2 # This gives us the middle index
+    value = array[index] # This gives us the median value
+    if index == value:
+        return True # Establish the base case for True
+    elif lo == hi:
+        return False # Estblish the base case for False
+    elif index > value:
+        return recurse_find_index(array, index + 1, hi)
+    else:
+        return recurse_find_index(array, lo, index)
+
+# Call the recursive method
 def find_index(array):
-    for i in range(0, len(array)):
-        if array[i] == i:
-            return True
-        elif array[i] > i:
-            return False
-    return False
+    return recurse_find_index(array, 0, len(array))
 
 # Test cases for find magic index.
 class MyTests(unittest.TestCase):
